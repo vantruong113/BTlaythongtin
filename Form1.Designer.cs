7d6a1f44d36eacbed447143ac50e3094f2f68fca
ADMIN <ADMIN@LAB1-MAY35>
2023-09-11 16:35:14 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace BTlaythongtin
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoEllipsis = true;
+            label1.AutoSize = true;
+            label1.BackColor = Color.WhiteSmoke;
+            label1.Font = new Font("Arial Black", 15F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.Red;
+            label1.Location = new Point(297, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(424, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Chương Trình Lấy Thông Tin Máy Tính ";
+            label1.TextAlign = ContentAlignment.TopRight;
+            label1.Visible = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(53, 100);
+            label2.Name = "label2";
+            label2.Size = new Size(130, 45);
+            label2.TabIndex = 1;
+            label2.Text = "NAME :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(53, 200);
+            label3.Name = "label3";
+            label3.Size = new Size(108, 45);
+            label3.TabIndex = 3;
+            label3.Text = "RAM :";
+            label3.Click += label3_Click_1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 26.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = Color.Blue;
+            label4.Location = new Point(257, 98);
+            label4.Name = "label4";
+            label4.Size = new Size(161, 47);
+            label4.TabIndex = 4;
+            label4.Text = "NBPHUC";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 26.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.ForeColor = Color.Blue;
+            label5.Location = new Point(237, 198);
+            label5.Name = "label5";
+            label5.Size = new Size(161, 47);
+            label5.TabIndex = 5;
+            label5.Text = "7990 MB";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(237, 277);
+            button1.Name = "button1";
+            button1.Size = new Size(218, 72);
+            button1.TabIndex = 6;
+            button1.Text = "THOÁT";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(751, 403);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Thông tin";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,31 @@
+namespace BTlaythongtin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+
+
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
